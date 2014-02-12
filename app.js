0e793d490a71b39da239156ffb069da0a243c45c
Fedor Indutny <fedor.indutny@gmail.com>
2014-02-13 01:22:27 +0400

    app: be compatible with latest blog

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var connect = require('connect');
 var path = require('path');
+var http = require('http');
 
 var app = connect()
     .use(connect.static(path.resolve(__dirname, 'public')));
 
-module.exports = app;
+module.exports = http.createServer(app);
